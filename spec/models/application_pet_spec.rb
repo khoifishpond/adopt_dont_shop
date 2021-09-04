9a29febd0249a5a65df92be9c850f0f8107d7506
Khoi Nguyen <khoinguyen311@gmail.com>
2021-09-03 21:31:10 -0700

    Add error message

--- spec/models/application_pet_spec.rb
+++ spec/models/application_pet_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 describe ApplicationPet do
   describe 'relationships' do
     it { should belong_to :application }
-    it {should belong_to :pet }
+    it { should belong_to :pet }
   end
 end
